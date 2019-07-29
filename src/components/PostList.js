3b64e36e6aaa9764620faf18f38b2374a4443c01
Vinicius <vinilitaiff@gmail.com>
2019-07-29 15:08:05 -0400

    add posts no state

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,22 +8,85 @@ class PostList extends Component {
       {
         id: 1,
         author: {
-          name: "Avatar 1",
-          avatar:
-            "https://conteudo.imguol.com.br/c/entretenimento/38/2017/12/21/cena-de-avatar-2009-1513852401735_v2_450x600.jpg"
+          name: 'Vanessa Romero',
+          avatar: 'https://i.pravatar.cc/150?img=1'
         },
-        date: "25 Jul 2019",
-        content: "Iae avatar",
+        date: '04 Jun 2019',
+        content: 'Pessoal, alguém sabe se a Rocketseat está contratando?',
         comments: [
           {
             id: 2,
             author: {
-              name: "Avatar 2",
-              avatar:
-                "https://www.tribunapr.com.br/wp-content/uploads/sites/1/2009/12/17-12-09_alm1171209.png?a86372"
+              name: 'Diego Fernandes',
+              avatar: 'https://avatars2.githubusercontent.com/u/2254731?v=4'
             },
-            date: "04 Jul 2019",
-            content: "Como vai avatar 1?"
+            date: '04 Jun 2019',
+            content:
+              'A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)'
+          }
+        ]
+      },
+      {
+        id: 3,
+        author: {
+          name: 'Neil Cook',
+          avatar: 'https://i.pravatar.cc/150?img=8'
+        },
+        date: '04 Jun 2019',
+        content:
+          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: 'Clara Lisboa',
+              avatar: 'https://i.pravatar.cc/150?img=5'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+          },
+          {
+            id: 5,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
+          }
+        ]
+      },
+      {
+        id: 6,
+        author: {
+          name: 'Gabriel Lisboa',
+          avatar: 'https://i.pravatar.cc/150?img=51'
+        },
+        date: '04 Jun 2019',
+        content:
+          'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 4,
+            author: {
+              name: 'Clara Lisboa',
+              avatar: 'https://i.pravatar.cc/150?img=5'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios construída!'
+          },
+          {
+            id: 5,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'https://i.pravatar.cc/150?img=11'
+            },
+            date: '04 Jun 2019',
+            content:
+              'Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes'
           }
         ]
       }
